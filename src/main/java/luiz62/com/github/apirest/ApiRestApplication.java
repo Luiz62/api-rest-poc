9911f56bb9cfa16c966a3f8d67aed7c26f4414fd
Luiz62 <luiztiagonascimento21@hotmail.com>
2024-07-29 15:23:57 -0300

    initial commit

---
+++ src/main/java/luiz62/com/github/apirest/ApiRestApplication.java
@@ -0,0 +1,13 @@
+package luiz62.com.github.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestApplication.class, args);
+    }
+
+}
