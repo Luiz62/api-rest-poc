2f65e8f80be1b522372391185aca1a8c38fdb26c
Luiz62 <luiztiagonascimento21@hotmail.com>
2024-07-29 16:18:26 -0300

    feat: add ObjectNotFoundException

--- src/main/java/luiz62/com/github/apirest/service/exceptions/ObjectNotFoundException.java
+++ src/main/java/luiz62/com/github/apirest/service/exceptions/ObjectNotFoundException.java
@@ -1,4 +1,14 @@
 package luiz62.com.github.apirest.service.exceptions;
 
-public class ObjectNotFoundException {
+import java.io.Serial;
+
+public class ObjectNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = -6660968059360526401L;
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
 }
+
