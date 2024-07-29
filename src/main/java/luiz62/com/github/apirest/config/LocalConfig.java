991f9eb815cd5e6e21891db3ca4799ee6e103733
Luiz62 <luiztiagonascimento21@hotmail.com>
2024-07-29 16:15:43 -0300

    fix: mockUser() in LocalConfig

--- src/main/java/luiz62/com/github/apirest/config/LocalConfig.java
+++ src/main/java/luiz62/com/github/apirest/config/LocalConfig.java
@@ -1,26 +1,34 @@
 package luiz62.com.github.apirest.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import luiz62.com.github.apirest.domain.UserEntity;
 import luiz62.com.github.apirest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.util.Arrays;
+import java.util.List;
 
+@Slf4j
 @Configuration
 @Profile(value = "local")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class LocalConfig {
+public class LocalConfig implements CommandLineRunner {
 
     public final UserRepository repository;
 
-    @Bean
     public void mockUser() {
         UserEntity u1 = new UserEntity(null, "Luiz", "luiz@gmail.com", "123");
         UserEntity u2 = new UserEntity(null, "Valdir", "valdir@gmail.com", "123");
-        repository.saveAll(Arrays.asList(u1, u2));
+        List<UserEntity> userEntities = repository.saveAll(Arrays.asList(u1, u2));
+        log.debug("Inserts users: {}", userEntities);
+    }
+
+    @Override
+    public void run(String... args) {
+        mockUser();
     }
 }
