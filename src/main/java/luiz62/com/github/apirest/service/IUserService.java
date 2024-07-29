2e4b1ac0c96399bbfbc41a25f4e0fb52069f3c4d
Luiz62 <luiztiagonascimento21@hotmail.com>
2024-07-29 16:46:02 -0300

    feat: add findAll in IUserService

--- src/main/java/luiz62/com/github/apirest/service/IUserService.java
+++ src/main/java/luiz62/com/github/apirest/service/IUserService.java
@@ -2,6 +2,10 @@ package luiz62.com.github.apirest.service;
 
 import luiz62.com.github.apirest.domain.entity.UserEntity;
 
+import java.util.List;
+
 public interface IUserService {
     UserEntity findById(Long id);
+
+    List<UserEntity> findAll();
 }
