f488b5053760792e0c55680d43c43a128842486a
Luiz62 <luiztiagonascimento21@hotmail.com>
2024-07-29 16:54:27 -0300

    feat: add findAll in UserResource

--- src/main/java/luiz62/com/github/apirest/resources/UserResource.java
+++ src/main/java/luiz62/com/github/apirest/resources/UserResource.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import luiz62.com.github.apirest.domain.dto.UserDTO;
 import luiz62.com.github.apirest.service.IUserService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "user")
@@ -23,5 +26,10 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok().body(mapper.map(service.findAll(), new TypeToken<List<UserDTO>>() {}.getType()));
     }
 }
