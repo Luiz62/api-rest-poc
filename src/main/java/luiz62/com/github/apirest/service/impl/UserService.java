fd646de0f70b7c9d542f408a7064fcba37b6d1d5
Luiz62 <luiztiagonascimento21@hotmail.com>
2024-07-29 15:58:17 -0300

    feat: add UserService

---
+++ src/main/java/luiz62/com/github/apirest/service/impl/UserService.java
@@ -0,0 +1,21 @@
+package luiz62.com.github.apirest.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import luiz62.com.github.apirest.domain.UserEntity;
+import luiz62.com.github.apirest.repository.UserRepository;
+import luiz62.com.github.apirest.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class UserService implements IUserService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserEntity findById(Long id) {
+        return repository.findById(id)
+                .orElse(null);
+    }
+}
