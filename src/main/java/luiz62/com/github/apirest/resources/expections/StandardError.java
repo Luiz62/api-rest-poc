767ffa0104a3f352675fc38fa1bc24afc37e1880
Luiz62 <luiztiagonascimento21@hotmail.com>
2024-07-29 16:22:14 -0300

    feat: add StandardError

--- src/main/java/luiz62/com/github/apirest/resources/expections/StandardError.java
+++ src/main/java/luiz62/com/github/apirest/resources/expections/StandardError.java
@@ -1,4 +1,21 @@
 package luiz62.com.github.apirest.resources.expections;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class StandardError {
+
+    private LocalDateTime timestamp;
+
+    private Integer status;
+
+    private String error;
+
+    private String path;
 }
