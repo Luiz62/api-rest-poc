73924c709d6805de0aa110d002ca2355de61cae9
Luiz62 <luiztiagonascimento21@hotmail.com>
2024-07-29 16:07:24 -0300

    feat: add mockUser() in LocalConfig

--- src/main/java/luiz62/com/github/apirest/config/LocalConfig.java
+++ src/main/java/luiz62/com/github/apirest/config/LocalConfig.java
@@ -1,9 +1,26 @@
 package luiz62.com.github.apirest.config;
 
+import lombok.RequiredArgsConstructor;
+import luiz62.com.github.apirest.domain.UserEntity;
+import luiz62.com.github.apirest.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.Arrays;
+
 @Configuration
 @Profile(value = "local")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class LocalConfig {
+
+    public final UserRepository repository;
+
+    @Bean
+    public void mockUser() {
+        UserEntity u1 = new UserEntity(null, "Luiz", "luiz@gmail.com", "123");
+        UserEntity u2 = new UserEntity(null, "Valdir", "valdir@gmail.com", "123");
+        repository.saveAll(Arrays.asList(u1, u2));
+    }
 }
